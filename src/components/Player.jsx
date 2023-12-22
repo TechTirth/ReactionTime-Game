68ac896adb118ae3f7b1f336f256ad627cd6d0e2
Tirth Vanparia <t1eerth.v@gmail.com>
2023-12-22 17:19:16 +0530

    Managing user input with state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayername, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event) => {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  };
+
+  const handleClick = () => {
+    setSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayername : "Unknown Entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayername} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
